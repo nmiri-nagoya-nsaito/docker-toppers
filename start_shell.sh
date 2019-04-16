6f611231678b51e193c70f7bf6719eeda7e64e0a
Naoki Saito <saito.naoki@nmiri.city.nagoya.jp>
2019-04-16 19:16:27 +0900

    add a service using ubuntu-cosmic

--- start_shell.sh
+++ start_shell.sh
@@ -3,6 +3,20 @@
 set -eu
 
 SERVICE=cli
+SCRIPT_DIR=$(cd $(dirname $0);pwd)
+
+if [ $# -ge 1 ]; then
+  SERVICE=$1
+fi
+
+echo "specified service: ${SERVICE}"
+
+if [ ! -d ${SCRIPT_DIR}/services/${SERVICE} ]; then
+  echo start_shell.sh: service \"${SERVICE}\" is not available.
+  echo "  available services:"
+  echo "    $(for i in $(ls services); do printf "\t%s\n" $i; done)"
+  exit 1
+fi
 
 if [ "x$(docker-compose ps -q ${SERVICE})" = "x" ]; then
   docker-compose build ${SERVICE}
