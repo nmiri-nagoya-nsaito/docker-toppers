4094d1491406e629660ceb9e5b466571d96e2161
Naoki Saito <saito.naoki@nmiri.city.nagoya.jp>
2020-03-10 18:47:47 +0900

    Shell commands commonly executed by each service are shared by files in common directory.

--- services/ubuntu-focal/scripts/root/setup_root.sh
+++ services/ubuntu-focal/scripts/root/setup_root.sh
@@ -2,12 +2,4 @@
 
 set -eu
 
-DEBIAN_FRONTEND=noninteractive apt-get install -y language-pack-ja lv nkf tzdata
-echo "Asia/Tokyo" > /etc/timezone
-rm /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
-update-locale LANGUAGE=ja_JP:ja LANG=ja_JP.UTF-8
-
-sed -i -e 's/\# store-plaintext-passwords = no/store-plaintext-passwords = no/' /etc/subversion/servers
-
 exit 0
